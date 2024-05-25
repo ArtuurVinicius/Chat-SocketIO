64a6a7d25d69b10d3c6f12a12aeb1d45b71823c9
ArtuurVinicius <117421458+ArtuurVinicius@users.noreply.github.com>
2024-05-25 16:05:24 +0000

    ft(align-message): alinhando a mensagem do usuário para a direita.

--- socketio/chat-socketio.js
+++ socketio/chat-socketio.js
@@ -1,25 +1,60 @@
-const express = require('express')
-const http = require('http')
-const socketIo = require('socket.io')
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const session = require('express-session');
 
-const app = express()
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-const server = http.createServer(app)
+// Configurar middleware para processar dados de formulários
+app.use(express.urlencoded({ extended: true }));
 
-const io = socketIo(server)
+// Configurar middleware para sessões
+app.use(session({
+  secret: 'secreto',
+  resave: false,
+  saveUninitialized: true
+}));
+
+const users = {};
 
 io.on("connection", socket => {
-  console.log("Usuário conectado", socket.id)
+  console.log("Usuário conectado", socket.id);
+
+  socket.on("username", username => {
+    users[socket.id] = username;
+  });
+
   socket.on("message", msg => {
-    console.log(msg)
-    io.emit("message", msg)
-  })
-})
+    console.log(msg);
+    const messageWithUsername = `${users[socket.id]}: ${msg}`;
+    io.emit("message", messageWithUsername);
+  });
+});
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/chat-socketio.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/chat-login.html");
+});
 
-// aqui vai o socketIo
+app.post("/chat", (req, res) => {
+  const username = req.body.username;
+  req.session.username = username;
+  res.redirect('/chat');
+});
 
-server.listen(3000)
+app.get('/chat', (req, res) => {
+  if (!req.session.username) {
+    return res.redirect('/');
+  }
+  res.sendFile(__dirname + '/chat-socketio.html');
+});
+
+app.get('/session-username', (req, res) => {
+  res.json({ username: req.session.username });
+});
+
+
+server.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
